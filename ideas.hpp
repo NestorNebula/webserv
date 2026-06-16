7c55f6f59f63856d6320c84fe90cb22c7dd549d5
Noa Houssier <nestornebula.dev@gmail.com>
2026-06-16 16:47:16 +0200

    chore: add first class ideas

---
+++ ideas.hpp
@@ -0,0 +1,29 @@
+// An active server listening for connections
+class Server {
+	// Connections
+	// Listening Socket
+};
+
+// A connection between a server and a client
+class Connection {
+	// Request
+	// Response
+	// Client Socket
+};
+
+class Request {
+	// Headers
+};
+
+class Response {
+	// Headers
+};
+
+// Socket abstraction
+class Socket {
+};
+
+// Request/Response headers
+class Headers {
+	// Key->Value pairs
+};
