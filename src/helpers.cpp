993e0c93f02f164cb41527e930c8b908497c8c7c
Noa Houssier <nestornebula.dev@gmail.com>
2026-06-21 15:57:17 +0200

    feat: add string helpers functions

---
+++ src/helpers.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   helpers.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nhoussie <nhoussie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/21 10:27:00 by nhoussie          #+#    #+#             */
+/*   Updated: 2026/06/21 15:13:17 by nhoussie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "helpers.hpp"
+#include <cctype>
+
+std::string trim(std::string s, std::string set) {
+	std::string::size_type start, end;
+	start = s.find_first_not_of(set);
+	end = s.find_last_not_of(set);
+	if (start == std::string::npos || end == std::string::npos)
+		return s;
+	return std::string(s, start, end - start + 1);
+}
+
+void capitalize(std::string &s) {
+	for (std::string::iterator it = s.begin(), ite = s.end(); it != ite; it++) {
+		if (it == s.begin() || !std::isalpha(*(it - 1)))
+			*it = std::toupper(*it);
+	}
+}
+
+std::string capitalize(const std::string &s) {
+	std::string copy(s);
+	capitalize(copy);
+	return copy;
+}
