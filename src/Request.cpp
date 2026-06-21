0ab455f8557c638d9ce45f5499121492ec31a4c1
Noa Houssier <nestornebula.dev@gmail.com>
2026-06-21 08:51:09 +0200

    feat: add methods to check members in Request

--- src/Request.cpp
+++ src/Request.cpp
@@ -26,24 +26,51 @@ bool Request::isInvalid() const {
 	return _state == INVALID;
 }
 
+bool Request::hasMethod() const {
+	return _state > START_LINE;
+}
+
 const std::string &Request::getMethod() const {
 	return _method;
+}
+
+bool Request::hasURL() const {
+	return _state > START_LINE;
 }
 
 const std::string &Request::getURL() const {
 	return _url;
 }
 
+bool Request::hasQuery() const {
+	return _state > START_LINE && !_query.empty();
+}
+
 const std::string &Request::getQuery() const {
 	return _query;
+}
+
+bool Request::hasVersion() const {
+	return _state > START_LINE;
 }
 
 const std::string &Request::getVersion() const {
 	return _version;
 }
 
+bool Request::hasHeader(const std::string &key) const {
+	return _state >= HEADERS && _headers.has(key);
+}
+bool Request::hasHeaders() const {
+	return _state >= HEADERS && _headers.size() > 0;
+}
+
 const Headers &Request::getHeaders() const {
 	return _headers;
+}
+
+bool Request::hasBody() const {
+	return _state == COMPLETE && !_body.empty();
 }
 
 const std::string &Request::getBody() const {
