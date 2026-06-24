4977fd901a9aab87f18d9a6626c05266559f2637
Noa Houssier <nestornebula.dev@gmail.com>
2026-06-24 14:37:18 +0200

    test: add tests for Response class

---
+++ tests/Response.cpp
@@ -0,0 +1,164 @@
+#include "MockResource.hpp"
+#include "Response.hpp"
+#include <gtest/gtest.h>
+
+using ::testing::ReturnRef;
+
+std::string defaultContent = "Content";
+
+static MockResource getResource(std::string &content = defaultContent) {
+	MockResource resource;
+	(void) content;
+
+	EXPECT_CALL(resource, getContent()).WillOnce(ReturnRef(defaultContent));
+	return resource;
+}
+
+TEST(Response, EmptyResponse) {
+	MockResource resource = getResource();
+	Response response(resource);
+
+	EXPECT_FALSE(response.isReady());
+}
+
+TEST(Response, MissingVersion) {
+	MockResource resource = getResource();
+	Response response(resource);
+
+	response.setCode(200);
+	response.setReason("OK");
+	EXPECT_FALSE(response.isReady());
+}
+
+TEST(Response, MissingCode) {
+	MockResource resource = getResource();
+	Response response(resource);
+
+	response.setVersion("HTTP/1.1");
+	response.setReason("OK");
+	EXPECT_FALSE(response.isReady());
+}
+
+TEST(Response, MissingReason) {
+	MockResource resource = getResource();
+	Response response(resource);
+
+	response.setVersion("HTTP/1.1");
+	response.setCode(200);
+	EXPECT_FALSE(response.isReady());
+}
+
+TEST(Response, Generate200) {
+	MockResource resource = getResource();
+	Response response(resource);
+
+	response.setVersion("HTTP/1.1");
+	response.setCode(200);
+	response.setReason("OK");
+	EXPECT_TRUE(response.isReady());
+	response.generate();
+	EXPECT_EQ(response.getRaw(), "HTTP/1.1 200 OK\r\n"
+			"\r\n"
+			"Content");
+}
+
+TEST(Response, Generate404) {
+	MockResource resource = getResource();
+	Response response(resource);
+
+	response.setVersion("HTTP/1.1");
+	response.setCode(404);
+	response.setReason("Not Found");
+	EXPECT_TRUE(response.isReady());
+	response.generate();
+	EXPECT_EQ(response.getRaw(), "HTTP/1.1 404 Not Found\r\n"
+			"\r\n"
+			"Content");
+}
+
+TEST(Response, GenerateOneHeader) {
+	MockResource resource = getResource();
+	Response response(resource);
+
+	response.setVersion("HTTP/1.1");
+	response.setCode(200);
+	response.setReason("OK");
+	response.addHeader(Header("Content-Type", "text/html"));
+	EXPECT_TRUE(response.isReady());
+	response.generate();
+	EXPECT_EQ(response.getRaw(), "HTTP/1.1 200 OK\r\n"
+			"Content-Type: text/html\r\n"
+			"\r\n"
+			"Content");
+}
+
+TEST(Response, GenerateMultipleHeaders) {
+	MockResource resource = getResource();
+	Response response(resource);
+	Headers headers;
+
+	response.setVersion("HTTP/1.1");
+	response.setCode(200);
+	response.setReason("OK");
+	headers.insert("Content-Type", "text/html");
+	headers.insert("Connection", "close");
+	headers.insert("Server", "webserv");
+	response.addHeaders(headers.begin(), headers.end());
+	EXPECT_TRUE(response.isReady());
+	response.generate();
+	EXPECT_EQ(response.getRaw(), "HTTP/1.1 200 OK\r\n"
+			"Connection: close\r\n"
+			"Content-Type: text/html\r\n"
+			"Server: webserv\r\n"
+			"\r\n"
+			"Content");
+}
+
+TEST(Response, GenerateEmptyBody) {
+	std::string content("");
+	MockResource resource = getResource(content);
+	Response response(resource);
+
+	response.setVersion("HTTP/1.1");
+	response.setCode(200);
+	response.setReason("OK");
+	EXPECT_TRUE(response.isReady());
+	response.generate();
+	EXPECT_EQ(response.getRaw(), "HTTP/1.1 200 OK\r\n"
+			"\r\n");
+}
+
+TEST(Response, GenerateMultilineBody) {
+	std::string content("Hello\nThere!\n");
+	MockResource resource = getResource(content);
+	Response response(resource);
+
+	response.setVersion("HTTP/1.1");
+	response.setCode(200);
+	response.setReason("OK");
+	EXPECT_TRUE(response.isReady());
+	response.generate();
+	EXPECT_EQ(response.getRaw(), "HTTP/1.1 200 OK\r\n"
+			"\r\n"
+			"Hello\n"
+			"There!\n");
+}
+
+TEST(Response, ActionAfterGenerate) {
+	MockResource resource = getResource();
+	Response response(resource);
+
+	response.setVersion("HTTP/1.1");
+	response.setCode(200);
+	response.setReason("OK");
+	EXPECT_TRUE(response.isReady());
+	response.generate();
+	EXPECT_EQ(response.getRaw(), "HTTP/1.1 200 OK\r\n"
+			"\r\n"
+			"Content");
+	EXPECT_FALSE(response.isReady());
+	EXPECT_THROW(response.setVersion("HTTP/1.0"), std::logic_error);
+	EXPECT_THROW(response.setCode(200), std::logic_error);
+	EXPECT_THROW(response.setReason("OK"), std::logic_error);
+	EXPECT_THROW(response.generate(), std::logic_error);
+}
