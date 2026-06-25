d5721abaa63db73e426c623fa626fcf438413ed6
Noa Houssier <nestornebula.dev@gmail.com>
2026-06-25 12:57:50 +0200

    feat: implement StaticResource class

--- include/StaticResource.hpp
+++ include/StaticResource.hpp
@@ -6,24 +6,44 @@
 /*   By: nhoussie <nhoussie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/25 09:42:42 by nhoussie          #+#    #+#             */
-/*   Updated: 2026/06/25 09:47:02 by nhoussie         ###   ########.fr       */
+/*   Updated: 2026/06/25 12:07:55 by nhoussie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include "Resource.hpp"
+#include <fstream>
 
 class StaticResource: public Resource {
 public:
-	StaticResource(const std::string &path);
+	StaticResource(const std::string &filepath): _filepath(filepath), _state(DEFAULT) {
+		_file.open(filepath.c_str());
+	}
+	~StaticResource() {
+		if (_file.is_open())
+			_file.close();
+	}
 	virtual void generate();
-	virtual bool done() const;
-	virtual bool inProgress() const;
-	virtual bool failed() const;
+	virtual bool done() const { return _state == DONE; }
+	virtual bool inProgress() const { return false; }
+	virtual bool failed() const { return _state == FAIL; }
 	virtual const std::string &getContent() const;
 
 private:
+	typedef enum eInternalState {
+		DEFAULT,
+		DONE,
+		FAIL,
+	} InternalState;
+
 	StaticResource(const StaticResource &);
 	StaticResource &operator=(const StaticResource &);
+
+	std::string _filepath;
+	InternalState _state;
+	std::string _content;
+	std::ifstream _file;
+
+	bool readContent();
 };
