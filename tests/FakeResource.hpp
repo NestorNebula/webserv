6f1da50c3928e5e500e5c6145ce9bbd7cdc2b3e8
Noa Houssier <nestornebula.dev@gmail.com>
2026-06-25 09:27:51 +0200

    feat: mock Resource methods in FakeResource

--- tests/FakeResource.hpp
+++ tests/FakeResource.hpp
@@ -1,4 +1,7 @@
+#pragma once
+
 #include "Resource.hpp"
+#include "gmock/gmock.h"
 
 class FakeResource: public Resource {
 public:
@@ -6,6 +9,10 @@ public:
 	  _content = content;
   }
   virtual ~FakeResource() {}
+  MOCK_METHOD0(generate, void());
+  MOCK_CONST_METHOD0(done, bool());
+  MOCK_CONST_METHOD0(inProgress, bool());
+  MOCK_CONST_METHOD0(failed, bool());
   virtual const std::string &getContent() const { return _content; }
   
 private:
