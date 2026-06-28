3a0c4690b9383492bed98ab1377cdbe6cf61a38e
Noa Houssier <nestornebula.dev@gmail.com>
2026-06-28 08:09:35 +0200

    feat: create HTML format for directory listing

--- src/DirectoryResource.cpp
+++ src/DirectoryResource.cpp
@@ -11,6 +11,8 @@
 /* ************************************************************************** */
 
 #include "DirectoryResource.hpp"
+#include <iostream>
+#include <sstream>
 #include <stdexcept>
 
 void DirectoryResource::generate() {
@@ -25,11 +27,38 @@ void DirectoryResource::generate() {
 const std::string &DirectoryResource::getContent() const {
 	if (_state != DONE)
 		throw std::logic_error("Content not available");
+	std::cout << "Content: " << _content;
 	return _content;
 }
 
 void DirectoryResource::buildList() {
+	std::ostringstream oss;
+
+	oss << "<!DOCTYPE html>\n"
+		"<html lang=\"en\">\n"
+		"<head>\n"
+		"<meta charset=\"utf-8\">\n"
+		"<title>Directory listing for " << _dirpath << "</title>\n"
+		"</head>\n"
+		"<body>\n"
+		"<h1>Directory listing for " << _dirpath << "</h1>\n"
+		"<hr>\n"
+		"<ul>\n";
+
 	dirent *dirFile;
-	while ((dirFile = readdir(_dir)) != NULL)
-		(_content += dirFile->d_name) += "\n";
+	while ((dirFile = readdir(_dir)) != NULL) {
+		std::string name(dirFile->d_name);
+		if (name != "." && name != "..") {
+			if (dirFile->d_type ==  DT_DIR)
+				name += '/';
+			oss << "<li>\n" 
+				"<a href=\"" << name << "\">" << name << "</a>\n"
+				"</li>\n";
+		}
+	}
+	oss << "</ul>\n"
+		"<hr>\n"
+		"</body>\n"
+	"</html>\n";
+	_content = oss.str();
 }
