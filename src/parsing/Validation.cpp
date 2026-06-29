0f234511f77414382138e9f732c9ab040ddf1aae
Maxime Marti <maxime@mamarti,fr>
2026-06-29 16:26:09 +0200

    chore: Add empty stubs for route, directive, size and validation

---
+++ src/parsing/Validation.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Validation.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mamarti <mamarti@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/29 15:35:01 by mamarti           #+#    #+#             */
+/*   Updated: 2026/06/29 16:08:26 by mamarti          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/ConfigParser.hpp"
+
+size_t	ConfigParser::parseSize(const std::string& sizeStr)
+{
+	(void) sizeStr;
+	return (0);
+}
+
+void	ConfigParser::validateServerConfig(const ServerConfig& server)
+{
+	(void) server;
+}
+
+void	ConfigParser::validateRouteConfig(const RouteConfig& route)
+{
+	(void) route;
+}
