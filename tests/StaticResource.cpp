1d0bf5d44209ef39405ce98b554612efbcf3550e
Noa Houssier <nestornebula.dev@gmail.com>
2026-06-30 09:55:14 +0200

    test: update StaticResource tests

--- tests/StaticResource.cpp
+++ tests/StaticResource.cpp
@@ -22,54 +22,62 @@ TEST(StaticResource, InvalidFilepath) {
 TEST(StaticResource, BasicFile) {
 	StaticResource resource("files/small.txt");
 	std::fstream fstream("files/small.txt");
-	std::ostringstream oss;
-	oss << fstream.rdbuf();
+	std::ostringstream oss1, oss2;
+	oss1 << fstream.rdbuf();
 
 	resource.generate();
+	oss2 << resource.stream().rdbuf();
+
 	EXPECT_TRUE(resource.done());
 	EXPECT_FALSE(resource.inProgress());
 	EXPECT_FALSE(resource.failed());
-	EXPECT_EQ(resource.getContent(), oss.str());
+	EXPECT_EQ(oss1.str(), oss2.str());
 }
 
 TEST(StaticResource, AsInterfacePtr) {
 	Resource *resource = new StaticResource("files/small.txt");
 	std::fstream fstream("files/small.txt");
-	std::ostringstream oss;
-	oss << fstream.rdbuf();
+	std::ostringstream oss1, oss2;
+	oss1 << fstream.rdbuf();
 
 	resource->generate();
+	oss2 << resource->stream().rdbuf();
+
 	EXPECT_TRUE(resource->done());
 	EXPECT_FALSE(resource->inProgress());
 	EXPECT_FALSE(resource->failed());
-	EXPECT_EQ(resource->getContent(), oss.str());
+	EXPECT_EQ(oss1.str(), oss2.str());
 	delete resource;
 }
 
 TEST(StaticResource, EmptyFile) {
 	StaticResource resource("files/empty.txt");
 	std::fstream fstream("files/empty.txt");
-	std::ostringstream oss;
-	oss << fstream.rdbuf();
+	std::ostringstream oss1, oss2;
+	oss1 << fstream.rdbuf();
 
 	resource.generate();
+	oss2 << resource.stream().rdbuf();
+
 	EXPECT_TRUE(resource.done());
 	EXPECT_FALSE(resource.inProgress());
 	EXPECT_FALSE(resource.failed());
-	EXPECT_EQ(resource.getContent(), oss.str());
+	EXPECT_EQ(oss1.str(), oss2.str());
 }
 
 TEST(StaticResource, LargeFile) {
 	StaticResource resource("files/large.txt");
 	std::fstream fstream("files/large.txt");
-	std::ostringstream oss;
-	oss << fstream.rdbuf();
+	std::ostringstream oss1, oss2;
+	oss1 << fstream.rdbuf();
 
 	resource.generate();
+	oss2 << resource.stream().rdbuf();
+
 	EXPECT_TRUE(resource.done());
 	EXPECT_FALSE(resource.inProgress());
 	EXPECT_FALSE(resource.failed());
-	EXPECT_EQ(resource.getContent(), oss.str());
+	EXPECT_EQ(oss1.str(), oss2.str());
 }
 
 TEST(StaticResource, MultipleGenerate) {
@@ -81,12 +89,12 @@ TEST(StaticResource, MultipleGenerate) {
 
 TEST(StaticResource, AccessBeforeGenerate) {
 	StaticResource resource("files/small.txt");
-	EXPECT_THROW(resource.getContent(), std::logic_error);
+	EXPECT_THROW(resource.stream(), std::logic_error);
 }
 
 TEST(StaticResource, AccessAfterFail) {
 	StaticResource resource("files/nosuchfile.txt");
 
 	resource.generate();
-	EXPECT_THROW(resource.getContent(), std::logic_error);
+	EXPECT_THROW(resource.stream(), std::logic_error);
 }
