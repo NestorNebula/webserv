e8e9835e0e068d2fc25fe11efc25ee5244b2ab53
Noa Houssier <nestornebula.dev@gmail.com>
2026-07-02 15:05:59 +0200

    feat: add prototypes for http utils functions

---
+++ include/http_utils.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   http_utils.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nhoussie <nhoussie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/02 13:08:43 by nhoussie          #+#    #+#             */
+/*   Updated: 2026/07/02 15:05:55 by nhoussie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include "ConfigParser.hpp" // Maxime's header
+
+bool isAllowedMethod(HttpMethod method, RouteConfig &config);
+
+bool isValidVersion(const std::string &version);
+
+RouteConfig *findBestRoute(const std::string &url, ServerConfig &config);
+
+std::string resolvePath(const std::string &url, RouteConfig &config);
+
+bool isExistingFile(const std::string &path);
+
+bool isDirectory(const std::string &path);
+
+bool isCgi(const std::string &path, RouteConfig &config);
+
+bool isAccessibleFile(const std::string &path);
