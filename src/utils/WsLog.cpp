e836df84a13c46e5503df631c81c9d346912adc3
mamarti <mamarti@f6r11s3.paris.42.school>
2026-07-10 10:38:44 +0200

    feat: Add config prefix

--- src/utils/WsLog.cpp
+++ src/utils/WsLog.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   WsLog.cpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kdonlon <kdonlon@student.42.fr>            +#+  +:+       +#+        */
+/*   By: mamarti <mamarti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/30 11:56:36 by kdonlon           #+#    #+#             */
-/*   Updated: 2026/07/03 09:51:46 by kdonlon          ###   ########.fr       */
+/*   Updated: 2026/07/10 10:34:16 by mamarti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,8 @@ static const std::string tgt_str[] =
     "conn  : ",
     "cgi   : ",
     "serv  : ",
-    "main  : "
+    "main  : ",
+	"cfg   : "
 };
 
 static const std::string &tgt_prefix(log_tgt tgt)
@@ -40,6 +41,8 @@ static const std::string &tgt_prefix(log_tgt tgt)
         return (tgt_str[5]);
     if (tgt & TGT_MAIN)
         return (tgt_str[6]);
+	if (tgt & TGT_PARSING)
+		return (tgt_str[7]);
 
     return (tgt_str[0]);
 }
