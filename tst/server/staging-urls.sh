34659343e95a26f5cb00fc3d5c353b7aaf7f8151
Kevin Donlon <kdonlon@student.42paris.fr>
2026-07-30 21:48:35 +0200

    bak

--- tst/server/staging-urls.sh
+++ tst/server/staging-urls.sh
@@ -1,14 +1,14 @@
-# http://127.0.0.1:8080/test.pl
-# http://127.0.0.1:8081/test.pl
-# http://127.0.0.1:8082/test.pl
+http://127.0.0.1:8080/test.pl
+http://127.0.0.1:8081/test.pl
+http://127.0.0.1:8082/test.pl
 
-# http://127.0.0.1:8080/test.py
-# http://127.0.0.1:8081/test.py
-# http://127.0.0.1:8082/test.py
+http://127.0.0.1:8080/test.py
+http://127.0.0.1:8081/test.py
+http://127.0.0.1:8082/test.py
 
-# http://127.0.0.1:8080/test.php
-# http://127.0.0.1:8081/test.php
-# http://127.0.0.1:8082/test.php
+http://127.0.0.1:8080/test.php
+http://127.0.0.1:8081/test.php
+http://127.0.0.1:8082/test.php
 
 http://127.0.0.1:8080/ka.php
 http://127.0.0.1:8081/ka.php
@@ -40,9 +40,9 @@ http://127.0.0.1:8082/ka.pl
 
 
 
-# http://127.0.0.1:8080/bigimage.php
-# http://127.0.0.1:8081/bigimage.php
-# http://127.0.0.1:8082/bigimage.php
+http://127.0.0.1:8080/bigimage.php
+http://127.0.0.1:8081/bigimage.php
+http://127.0.0.1:8082/bigimage.php
 
 
 # http://127.0.0.1:8080/bigaudio.php
