98a08d3dfc022daddaf9a5b444634ef32379db7a
Kevin Donlon <kdonlon@student.42.fr>
2026-08-13 16:03:03 +0200

    buggy

---
+++ tst/server/www/to.php
@@ -0,0 +1,14 @@
+<?php
+header('Content-type: text/plain');
+// header('Connection: keep-alive');
+// header('Content-length: 23');
+flush();
+?>
+I'm not dead yet.
+<?php
+flush();
+// sleep(5);
+?>
+fuck
+
+
