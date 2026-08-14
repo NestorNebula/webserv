ed2e595289870cbfe0f83f7c28da5b38d0fb1298
Kevin Donlon <kdonlon@student.42paris.fr>
2026-08-14 11:37:52 +0200

    ADD : nhoussie - demo

---
+++ demo/main.cpp
@@ -0,0 +1,55 @@
+#include "ConfigParser.hpp"
+#include "Epoll.hpp"
+#include "Server.hpp"
+#include "unistd.h"
+#include <iostream>
+
+static bool setWorkingDirectory(const std::string &path);
+
+static std::string getConfigFileName(const std::string &path);
+
+int main(int argc, char *argv[], char **envp) {
+  WsLog::tgt = (TGT_SERV | TGT_HTTP);
+  WsLog::lvl = LVL_ALL;
+  if (argc < 2) {
+    std::cerr << "usage: demo <config>\n";
+    return 0;
+  }
+
+  if (!setWorkingDirectory(argv[1])) {
+    std::cerr << "couldn't setup working directory.\n";
+    return 0;
+  }
+  ConfigParser parser;
+  try {
+	parser.parseFile(getConfigFileName(argv[1]));
+  } catch (std::exception &e) {
+	  std::cout << e.what() << "\n";
+	  return 0;
+  }
+  const std::vector<ServerConfig> &servers = parser.getServers();
+
+  Epoll epoll(envp);
+  for (std::vector<ServerConfig>::const_iterator it = servers.begin(),
+                                                 ite = servers.end();
+       it != ite; it++)
+    new Server(&epoll, it->port, *it);
+  epoll.loop();
+
+  return 0;
+}
+
+static bool setWorkingDirectory(const std::string &path) {
+  std::string::size_type lastSlash = path.find_last_of('/');
+  if (lastSlash == std::string::npos)
+    return true;
+  std::string directory = path.substr(0, lastSlash);
+  return chdir(directory.c_str()) == 0;
+}
+
+static std::string getConfigFileName(const std::string &path) {
+	std::string::size_type lastSlash = path.find_last_of('/');
+	if (lastSlash == std::string::npos)
+		return path;
+	return path.substr(lastSlash + 1);
+}
