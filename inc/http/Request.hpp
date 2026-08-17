2ee8f0f9572347d0347873222ceccc2ac263e80b
Noa Houssier <nestornebula.dev@gmail.com>
2026-08-17 11:00:34 +0200

    fix: update hasBody value for incomplete request

--- inc/http/Request.hpp
+++ inc/http/Request.hpp
@@ -48,7 +48,7 @@ public:
   bool hasHeaders() const { return _state >= HEADERS && _headers.size() > 0; }
   bool headersComplete() const { return _state > HEADERS; }
   const Headers &getHeaders() const { return _headers; }
-  bool hasBody() const { return _state == COMPLETE && _bodySize > 0; }
+  bool hasBody() const { return _body != NULL && _bodySize > 0; }
   Stream *getBody() {
     if (_body == NULL)
       throw std::logic_error("accessing null body Stream");
