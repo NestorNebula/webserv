951cde5c28355f3d5400380ff53b6287511466a3
Kevin Donlon <kdonlon@student.42paris.fr>
2026-08-23 16:03:42 +0200

    root : Makefile, main.cpp

---
+++ Makefile
@@ -0,0 +1,117 @@
+NAME=webserv
+
+CC=c++
+CFLAGS=-Wall -Wextra -Werror -std=c++98 -MMD -MP -O3
+# -Wno-unused
+# -Wshadow=local
+
+GIT_DIR=.
+
+INC_DIR=$(GIT_DIR)/inc
+SRC_DIR=$(GIT_DIR)/src
+OBJ_DIR=.obj
+LIB_DIR=.
+
+CFLAGS+=-I . \
+	-I $(INC_DIR) \
+	-I $(INC_DIR)/server \
+	-I $(INC_DIR)/utils \
+	-I $(INC_DIR)/http \
+	-I $(INC_DIR)/streams \
+	-I $(INC_DIR)/resources
+
+LFLAGS=
+LIBS =
+
+ifdef DBG
+DEFS += -DDBG_CANON="$(DBG)"
+endif
+
+ifdef TEST
+DEFS += -DTEST="$(TEST)"
+endif
+
+FILES = \
+	utils/WsLog.cpp \
+	utils/helpers.cpp \
+	utils/http_utils.cpp \
+	utils/libft.cpp \
+	utils/FilePath.cpp \
+	http/Headers.cpp \
+	http/Request.cpp \
+	http/Response.cpp \
+	http/Session.cpp \
+	resources/StaticResource.cpp \
+	resources/DirectoryResource.cpp \
+	resources/BuiltinResource.cpp \
+	streams/Stream.cpp \
+	streams/TemporaryFileStream.cpp \
+	server/EpollClient.cpp \
+	server/Epoll.cpp \
+	server/Server.cpp \
+	server/Socket.cpp \
+	server/Connection.cpp \
+	server/CgiEnv.cpp \
+	server/CgiPipe.cpp \
+	server/ResourceCgi.cpp \
+	server/ResourcePiped.cpp \
+	server/ResourceFcgi.cpp \
+	server/FcgiMsg.cpp \
+	server/FcgiConn.cpp \
+	server/FcgiPipe.cpp \
+	parsing/ConfigParser.cpp \
+	parsing/ParseBlocks.cpp \
+	parsing/ParserTools.cpp \
+	parsing/Tokenizer.cpp \
+	parsing/Validation.cpp 
+
+TPP = 
+
+SRCS = $(addprefix $(SRC_DIR)/, $(FILES))
+OBJS = $(addprefix $(OBJ_DIR)/, $(FILES:.cpp=.o)) \
+		$(OBJ_DIR)/main.o
+DEPS = $(OBJS:.o=.d)
+
+# alternative approach
+# OBJ = $(patsubst $(SRC_DIR)/%.cpp,$(OBJ_DIR)/src/%.o,$(SRC)) \
+# 	  $(patsubst %.cpp,$(OBJ_DIR)/%.o,$(DEMO_SRC))
+
+RM = rm -f
+RMDIR = rm -fr
+MKDIR = mkdir -p
+
+NULL :=
+\t   := $(NULL)	$(NULL)
+
+
+all: $(NAME)
+
+$(NAME): $(OBJS) $(TPP)
+	$(CC) $(DEFS) $(CFLAGS) $(LFLAGS) $(OBJS) $(LIBS) -o $@
+
+$(OBJ_DIR)/%.o: $(SRC_DIR)/%.cpp $(INC_DIR)/%.hpp 
+	@$(MKDIR) $(dir $@)
+	@echo "$(<F)$(\t)=>$(\t)$(@F)"
+	@$(CC) $(DEFS) $(CFLAGS) -c $< -o $@ 
+
+$(OBJ_DIR)/%.o: %.cpp 
+	@$(MKDIR) $(dir $@)
+	@echo "$(<F)$(\t)=>$(\t)$(@F)"
+	@$(CC) $(DEFS) $(CFLAGS) -c $< -o $@ 
+	
+$(OBJ_DIR)/%.o: $(SRC_DIR)/%.cpp 
+	@$(MKDIR) $(dir $@)
+	@echo "$(<F)$(\t)=>$(\t)$(@F)"
+	@$(CC) $(DEFS) $(CFLAGS) -c $< -o $@ 
+
+clean:
+	@$(RMDIR) $(OBJ_DIR)
+
+fclean: clean
+	@$(RM) $(NAME)
+
+re: fclean $(NAME)
+
+-include $(DEPS)
+
+.PHONY:  all clean fclean re
---
+++ main.cpp
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kdonlon <kdonlon@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/19 11:24:22 by kdonlon           #+#    #+#             */
+/*   Updated: 2026/08/23 09:43:49 by kdonlon          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "WsLog.hpp"
+
+#include "Epoll.hpp"
+#include "Server.hpp"
+#include "Connection.hpp"
+#include "ConfigParser.hpp"
+#include "FilePath.hpp"
+
+#include <deque>
+
+int main (int argc, char ** argv, char **envp)
+{   
+    WsLog::kd();
+    // WsLog::nh();
+    // WsLog::mm();
+
+    if (argc < 2)
+    {
+        std::cerr << "usage: demo <config>\n";
+        return 0;
+    }
+    if (argc > 2)
+    {
+        switch(argv[2][0])
+        {
+        case '0':
+           WsLog::tgt = TGT_NONE;
+           break;
+        case 'k':
+           WsLog::tgt = TGT_KD;
+           break;
+        case 'a':
+           WsLog::tgt = TGT_ALL; //  & ~(TGT_CGI_HEAD | TGT_CGI_DATA);
+           break;
+        }
+    }
+// #kd - conf_file_root
+    std::string conf_root;
+    if (env_pwd(envp, conf_root))
+    {
+        WSLOG(LVL_ERR, TGT_MAIN, "couldn't detect working directory");
+        return (0);
+    }
+    if (!setWorkingDirectory(argv[1], conf_root)) 
+    {
+        WSLOG(LVL_ERR, TGT_MAIN, "couldn't setup working directory");
+        return 0;
+    }
+    
+// #kd - conf_file_root
+    ConfigParser parser(conf_root);
+    try 
+    {
+        parser.parseFile(getConfigFileName(argv[1]));
+    } 
+    catch (std::exception &e) 
+    {
+        WSLOG(LVL_ERR, TGT_MAIN, "ex: main\n", e.what());
+        return 0;
+    }
+
+    const std::vector<ServerConfig> &servers = parser.getServers();
+
+    int     err = 0;
+    Epoll   *ep = NULL;
+    
+    try
+    {
+        ep = new Epoll(envp);
+       
+        err = ep->serve(servers);
+        if (err)
+          err = ep->loop();
+        WSLOG(LVL_ERR, TGT_MAIN, "exit: ", err);
+    }
+    catch(const std::exception& e)
+    {
+        WSLOG(LVL_ERR, TGT_MAIN, "ex: main\n", e.what());
+    }
+
+    delete (ep);
+    return (err);
+}
