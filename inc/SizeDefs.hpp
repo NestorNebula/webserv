31d467318e616a206a2b0fc07042b392f8b2b2df
Kevin Donlon <kdonlon@student.42.fr>
2026-08-25 19:34:56 +0200

    ka : in progress

--- inc/SizeDefs.hpp
+++ inc/SizeDefs.hpp
@@ -6,7 +6,7 @@
 /*   By: kdonlon <kdonlon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/20 17:56:17 by kdonlon           #+#    #+#             */
-/*   Updated: 2026/08/25 08:50:19 by kdonlon          ###   ########.fr       */
+/*   Updated: 2026/08/25 19:28:39 by kdonlon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,8 +47,8 @@
 
 
 # ifndef RES_CGI_WAIT_COMPLETE
-    // .. except if content-length ... 
-#  define RES_CGI_WAIT_COMPLETE 1
+    // .. except if content-length > xxx
+#  define RES_CGI_WAIT_COMPLETE 0
 # endif
 
 
--- src/server/ResourcePiped.cpp
+++ src/server/ResourcePiped.cpp
@@ -6,7 +6,7 @@
 /*   By: kdonlon <kdonlon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/21 00:16:10 by kdonlon           #+#    #+#             */
-/*   Updated: 2026/08/25 09:04:20 by kdonlon          ###   ########.fr       */
+/*   Updated: 2026/08/25 19:31:18 by kdonlon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,11 +73,11 @@ int	ResourcePiped::status(void)
 #if RES_CGI_WAIT_COMPLETE
         if (this->resp.size())
             return (1);
-#endif
 // KEEP_ALIVE
-		// where is chk_rsp_len : wait
+		// if (something)
 		return (RSP_KPALIVE);
-		// return (RSP_COMPLETE);
+#endif
+		return (RSP_COMPLETE);
 	}
 	
 	WSLOG(LVL_DBG, TGT_RSRC_STAT, "stat:  (need data)");
