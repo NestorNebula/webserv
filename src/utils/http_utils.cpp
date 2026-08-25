025109776e432526d181fe1e361933a5dfd81780
Noa Houssier <nestornebula.dev@gmail.com>
2026-08-25 11:31:20 +0200

    feat: add getCookie util function

--- src/utils/http_utils.cpp
+++ src/utils/http_utils.cpp
@@ -13,6 +13,7 @@
 #include "utils/http_utils.hpp"
 #include "utils/http_utils.hpp"
 #include <sys/stat.h>
+#include <algorithm>
 #include <unistd.h>
 
 bool isAllowedMethod(HttpMethod method, RouteConfig &config) {
@@ -279,4 +280,19 @@ std::string normalizeURI(const std::string &uri) {
   if (!endSlash)
     normalized.erase(normalized.size() - 1);
   return normalized;
+}
+
+std::string getCookie(const std::string &cookies, const std::string &key) {
+  std::vector<std::string> splitCookies(split(cookies, "; "));
+  std::string val;
+
+  for (std::vector<std::string>::const_iterator it = splitCookies.begin(), ite = splitCookies.end(); val.empty() && it != ite; it++) {
+    std::string cookie = *it;
+    if (std::count(cookie.begin(), cookie.end(), '=') == 1) {
+      std::string::size_type pos = cookie.find('=');
+      if (pos != 0 && pos != cookie.size() - 1 && cookie.substr(0, pos) == key)
+        val = cookie.substr(pos + 1);
+    }
+  }
+  return val;
 }
