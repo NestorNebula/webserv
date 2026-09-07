f320ad9ac38382dde07d9fe8db36522c83ab3e0c
Kevin Donlon <kdonlon@student.42paris.fr>
2026-09-07 11:45:26 +0200

    main : pre-rebase-main

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: kdonlon <kdonlon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/19 11:24:22 by kdonlon           #+#    #+#             */
-/*   Updated: 2026/09/07 10:08:00 by kdonlon          ###   ########.fr       */
+/*   Updated: 2026/09/07 11:45:10 by kdonlon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 #include "Epoll.hpp"
 
 #include "WsTime.hpp"
+
 int main (int argc, char ** argv, char **envp)
 {
     WsLog::kd();
