b8db8b3b537a77af7131a1c3d3cc073f15995246
Kevin Donlon <kdonlon@student.42paris.fr>
2026-09-07 13:10:39 +0200

    update : www/kd

---
+++ www/kd/exit.py
@@ -0,0 +1,14 @@
+from __future__ import print_function
+
+import sys
+import os
+
+import cgi
+import cgitb
+
+
+print("Content-Type: text/plain", end="\r\n\r\n");
+print("PYTHON : will exit (3)\r\n");
+
+exit (3);
+
