0e6bd1412d1d6c4a86945054b586fc41886d42a7
Noa Houssier <nestornebula.dev@gmail.com>
2026-09-10 14:58:30 +0200

    fix: set WITH_RETRY condition correctly

--- inc/http/Session.hpp
+++ inc/http/Session.hpp
@@ -38,7 +38,7 @@ public:
     WRSOCK,  // Write to Connection socket
     CLOSE,   // Close the Connection
     KPALIVE, // Keep the Connection alive
-#ifdef WITH_RETRY
+#if WITH_RETRY
     RETRY, // Retry to handle the Request
 #endif
   } Action;
