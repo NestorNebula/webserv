ec69dd25a25504c29d19a59cd7ca11bb24d1fa53
Kevin Donlon <kdonlon@student.42paris.fr>
2026-09-13 15:49:25 +0200

    Request.hpp : fix : uninitialized values

--- inc/http/Request.hpp
+++ inc/http/Request.hpp
@@ -6,7 +6,7 @@
 /*   By: kdonlon <kdonlon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/20 08:56:52 by nhoussie          #+#    #+#             */
-/*   Updated: 2026/09/12 14:40:42 by kdonlon          ###   ########.fr       */
+/*   Updated: 2026/09/13 15:48:53 by kdonlon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,9 @@
 
 class Request {
 public:
-  Request() : _state(EMPTY), _body(NULL), _bodySize(0) {
+// #kd
+// Conditional jump or move depends on uninitialised value(s)
+  Request() : _state(EMPTY), _method(METHOD_UNKNOWN), _body(NULL), _bodySize(0) {
     WSLOG(LVL_DBG, TGT_REQ, "Request constructor");
   }
   ~Request() {
@@ -73,7 +75,6 @@ private:
     COMPLETE,
     INVALID,
   } InternalState;
-
   std::string _raw;
   InternalState _state;
   HttpMethod _method;
