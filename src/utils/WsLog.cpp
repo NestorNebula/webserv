48149238e00642cc58ddb067f9939f6d8698b5a5
Kevin Donlon <kdonlon@student.42paris.fr>
2026-09-13 15:47:44 +0200

    WsLog : clean

--- src/utils/WsLog.cpp
+++ src/utils/WsLog.cpp
@@ -6,7 +6,7 @@
 /*   By: kdonlon <kdonlon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/30 11:56:36 by kdonlon           #+#    #+#             */
-/*   Updated: 2026/09/13 10:41:47 by kdonlon          ###   ########.fr       */
+/*   Updated: 2026/09/13 15:43:28 by kdonlon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -361,14 +361,14 @@ void    WsLog::kd(void)
         | LVL_ALL
     ;
     WsLog::tgt = TGT_NONE
-        | TGT_EPOLL
-        | TGT_EPOLL_EVT
-        | TGT_EPOLL_CTL
+        // | TGT_EPOLL
+        // | TGT_EPOLL_EVT
+        // | TGT_EPOLL_CTL
 
         // | TGT_TIMEO
         // | TGT_ALL
         | TGT_SERV_ALL
-        | TGT_EPC
+        // | TGT_EPC
         // | TGT_EPC_RECV
         // | TGT_EPC_SEND
 
