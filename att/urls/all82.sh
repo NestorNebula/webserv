9725d1e00f24fa06773052624498cc69edc55555
Kevin Donlon <kdonlon@student.42.fr>
2026-09-14 11:10:24 +0200

    att

--- att/urls/all82.sh
+++ att/urls/all82.sh
@@ -1,24 +1,15 @@
 
-http://127.0.0.1:8081/cgi-vars/vars.py
 http://127.0.0.1:8082/cgi-vars/vars.py
 
-http://127.0.0.1:8081/cgi-vars/vars.php
 http://127.0.0.1:8082/cgi-vars/vars.php
 
 
-http://127.0.0.1:8081/cgi-big/bigtext.php
 http://127.0.0.1:8082/cgi-big/bigtext.php
 
-http://127.0.0.1:8081/cgi-big/bigimage.php
 http://127.0.0.1:8082/cgi-big/bigimage.php
 
-http://127.0.0.1:8081/cgi-big/bigaudio.php
 http://127.0.0.1:8082/cgi-big/bigaudio.php
 
-
-http://127.0.0.1:8081/files/e4.jpg
-http://127.0.0.1:8081/files/earth.jpg
-http://127.0.0.1:8081/files/Kanan.mp3
 
 http://127.0.0.1:8082/files/e4.jpg
 http://127.0.0.1:8082/files/earth.jpg
