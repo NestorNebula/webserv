59aff721d745f7645ab459f2eb4b99c00edf8acb
Kevin Donlon <kdonlon@student.42paris.fr>
2026-09-14 17:54:56 +0200

    cgi-uploads : add BACK href

--- www/kd/cgi-uploads/ul.php
+++ www/kd/cgi-uploads/ul.php
@@ -1,7 +1,8 @@
 <?php
+    define('WSEOL', "<br>");
+    header('Content-type: text/html');
 
-    header('Content-type: text/plain');
-
+    // print('<html>');
     if (isset($_FILES['file']))
     {
         $file_name = $_FILES['file']['name'];
@@ -9,27 +10,28 @@
         if (file_exists($file_path))
         {
             header('Status: 409');
-            print("PHP  : upload\n\n");
-            print("PHP  : file exists\n");
+            print("PHP  : upload" . WSEOL);
+            print("PHP  : file exists" . WSEOL);
+            print("<a href='javascript:history.back();'>BACK</a>" . WSEOL);
             return (0);
         }
 
-        print("PHP  : upload\n\n");
-        echo ("file : " . $file_name) . PHP_EOL;
-        echo ("path : " . $file_path) . PHP_EOL;
+        print("PHP  : upload" . WSEOL);
+        echo ("file : " . $file_name) . WSEOL;
+        echo ("path : " . $file_path) . WSEOL;
         switch ($_FILES['file']['error']) {
             case UPLOAD_ERR_OK:
-                // echo "Success\n";
+                // echo "Success" . WSEOL
                 break;
             case UPLOAD_ERR_NO_FILE:
-                echo "No file\n";
+                echo "No file" . WSEOL;
                 break;
             case UPLOAD_ERR_INI_SIZE:
             case UPLOAD_ERR_FORM_SIZE:
-                echo "Exceeded filesize limit\n";
+                echo "Exceeded filesize limit" . WSEOL;
                 break;
             case UPLOAD_ERR_NO_TMP_DIR:
-                echo "No tmp dirrectory\n";
+                echo "No tmp dirrectory" . WSEOL;
                 break;
             default:
                 echo "Unknown error " . $_FILES['file']['error'];
@@ -39,8 +41,8 @@
     }
     else
     {
-        print("PHP : upload\n\n");
-        print("PHP : no file set\n");
-
+        print("PHP : upload" . WSEOL);
+        print("PHP : no file set" . WSEOL);
     }
+    print("<a href='javascript:history.back();'>BACK</a>" . WSEOL);
 ?>
