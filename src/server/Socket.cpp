a667d934960620575956cea0b375d1652575a522
Kevin Donlon <kdonlon@student.42paris.fr>
2026-09-14 17:35:52 +0200

    fcgi : test (fcgi_sock) is truly a socket file

--- src/server/Socket.cpp
+++ src/server/Socket.cpp
@@ -6,11 +6,15 @@
 /*   By: kdonlon <kdonlon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/20 22:16:45 by kdonlon           #+#    #+#             */
-/*   Updated: 2026/09/03 21:34:53 by kdonlon          ###   ########.fr       */
+/*   Updated: 2026/09/14 17:34:24 by kdonlon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Socket.hpp"
+
+#include <unistd.h>
+#include <sys/stat.h>
+#include <stdio.h>
 
 int sock_non_block(int fd)
 {
@@ -27,7 +31,7 @@ std::string addr_2_str(struct sockaddr_in *addr)
     // ntohl (addr->sin_addr.s_addr);
 	unsigned char *a = reinterpret_cast<unsigned char*>(&addr->sin_addr.s_addr);
     ss << (int) a[0] << "." << (int) a[1] << "."<< (int) a[2] << "."<< (int) a[3] << ":" << ntohs(addr->sin_port);
-    
+
     return (ss.str());
 }
 
@@ -39,4 +43,15 @@ int fd_close(int *fd)
 	int err = close(*fd);
 	*fd = -1;
     return (err);
+}
+
+int sock_file(const char *path)
+{
+    if (access(path, R_OK | W_OK))
+        return (0);
+
+    struct stat st;
+    if (stat(path, &st))
+        return (0);
+    return ((st.st_mode & S_IFMT) == S_IFSOCK);
 }
