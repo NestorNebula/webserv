b3c928768773a5d6e23af831809cca4a01f6531c
Kevin Donlon <kdonlon@student.42paris.fr>
2026-09-14 18:01:17 +0200

    att : perl-only, python-only

---
+++ att/urls/python.sh
@@ -0,0 +1,11 @@
+
+# PYTHON
+
+http://127.0.0.1:8081/cgi-vars/vars.py
+http://127.0.0.1:8081/cgi-codes/stat.py
+# http://127.0.0.1:8081/cgi-uploads/ul.py -- Forbidden
+
+
+http://127.0.0.1:8082/cgi-vars/vars.py
+http://127.0.0.1:8082/cgi-codes/stat.py
+http://127.0.0.1:8082/cgi-uploads/ul.py
