721a655d3e4d4a58930284283f4c0f6ba4b5c74e
Kevin Donlon <kdonlon@student.42paris.fr>
2026-09-14 23:43:51 +0200

    att : more urls

---
+++ att/urls/all80.sh
@@ -0,0 +1,11 @@
+http://127.0.0.1:8080/about.html
+http://127.0.0.1:8080/cookie.html
+http://127.0.0.1:8080/contact.html
+http://127.0.0.1:8080/index.html
+http://127.0.0.1:8080/delete.html
+
+http://127.0.0.1:8080/assets/fonts/Inter/Inter-Italic-VariableFont_opsz,wght.ttf
+http://127.0.0.1:8080/assets/fonts/Inter/Inter-VariableFont_opsz,wght.ttf
+
+http://127.0.0.1:8080/assets/fonts/JetBrains_Mono/JetBrainsMono-Italic-VariableFont_wght.ttf
+http://127.0.0.1:8080/assets/fonts/JetBrains_Mono/JetBrainsMono-VariableFont_wght.ttf
