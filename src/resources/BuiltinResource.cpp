7a7a18b4cba7a63e320c433996b7cb87dde7f988
Kevin Donlon <kdonlon@student.42.fr>
2026-09-15 14:13:14 +0200

    201 : prettier html

--- src/resources/BuiltinResource.cpp
+++ src/resources/BuiltinResource.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   BuiltinResource.cpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nhoussie <nhoussie@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kdonlon <kdonlon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/12 09:10:36 by nhoussie          #+#    #+#             */
-/*   Updated: 2026/07/12 15:00:41 by nhoussie         ###   ########.fr       */
+/*   Updated: 2026/09/15 14:05:39 by kdonlon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,14 +64,30 @@ void BuiltinResource::buildHTMLFromCode() {
               "<meta charset=\"utf-8\">\n"
               "<meta name=\"viewport\" content=\"width=device-width, "
               "initial-scale=1.0\" />\n"
+              "<link rel=\"stylesheet\" href=\"/css/styles.css\" />\n"
               "<title>"
            << _code << " " << reason
            << "</title>\n"
               "</head>\n"
               "<body>\n"
-              "<h1>"
-           << _code << " " << reason << "</h1>\n"
+
+<< "<header>\n"
+		<< "<h1>WEBSERV</h1>\n"
+		<< "<p>Demonstration Website</p>\n"
+		<< "<p>for our HTTP server</p>\n"
+    << "</h1>\n"
+<< "</header>\n"
+	<< "<main>\n"
+
+		<< "<h1 style=\"text-align:center;color:#16db65\">" << _code << " " << reason << "</h1>\n"
            << div.str()
+
+  << "<button class=\"request\" onclick=\"history.back();\">"
+	<< "<p class=\"method\">BACK</p>"
+<< "</button>"
+	<< "</main>>\n"
+
+              // "<h1>"
            << "</body>\n"
               "</html>\n";
 }
